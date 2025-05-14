6ca1ae3a0e88693342a9ec27957cae9a6dd4a124
Matthew Buckton <matthew@buckton.org>
2025-05-14 18:31:53 +1000

    update copyright

--- src/test/java/io/mapsmessaging/utilities/threads/tasks/ConcurrentPriorityTaskSchedulerJMH.java
+++ src/test/java/io/mapsmessaging/utilities/threads/tasks/ConcurrentPriorityTaskSchedulerJMH.java
@@ -1,9 +1,10 @@
 /*
- *    Copyright [ 2020 - 2021 ] [Matthew Buckton]
+ *    Copyright [ 2020 - 2024 ] [Matthew Buckton]
+ *    Copyright [ 2024 - 2025 ] [Maps Messaging B.V.]
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
  *
  *        http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,7 +13,6 @@
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
- *
  *
  */
 
